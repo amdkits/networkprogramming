dadb86f286e81fc3ecfa7f74990ea0264552d894
amdkits <kits.amd@gmail.com>
2026-02-24 03:06:32 +0530

    ADD comments to understand

--- showip.c
+++ showip.c
@@ -2,26 +2,26 @@
 ** showip.c -- modern version
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <sys/types.h>
+#include <stdio.h> // fprintf, printf
+#include <stdlib.h> // free, malloc
+#include <string.h> // memset
+#include <arpa/inet.h>// inet_ntop
+#include <netdb.h> // getaddrinfo, struct addrinfo
+#include <sys/types.h> // AF_INET, SOCK_STREAM, struct sockaddr
 #include <sys/socket.h>
 
 int main(int argc, char *argv[])
 {
-    struct addrinfo hints, *res, *p;
-    int status;
-    char ipstr[INET6_ADDRSTRLEN];
+    struct addrinfo hints, *res, *p; // hint is shittonne of data and res returns pointer to the first LL 
+    int status; // stores return value from ADDRINFO
+    char ipstr[INET6_ADDRSTRLEN]; // allocates a character buffer large enough to hold largest possible IPv6 string
 
     if (argc != 2) {
         fprintf(stderr,"usage: showip hostname\n");
         return 1;
     }
 
-    memset(&hints, 0, sizeof hints);
+    memset(&hints, 0, sizeof hints); // ai_family, ai_flags {0}
     hints.ai_family = AF_UNSPEC;      // IPv4 or IPv6
     hints.ai_socktype = SOCK_STREAM;  // TCP (doesn't really matter here)
 
@@ -30,16 +30,23 @@ int main(int argc, char *argv[])
         return 2;
     }
 
+    /*
+     argv[1] hostname string
+     NULL no port specified
+     &hints pointer to filter struct
+     &res pointer to pointer, function will write into this
+     */
+
     printf("IP addresses for %s:\n\n", argv[1]);
 
     for (p = res; p != NULL; p = p->ai_next) {
-        void *addr;
-        char *ipver;
+        void *addr; // pointer to raw binary IP
+        char *ipver; // string label IPv4 IPv6
 
         if (p->ai_family == AF_INET) { // IPv4
-            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
-            addr = &(ipv4->sin_addr);
-            ipver = "IPv4";
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr; // cast generic sockaddr pointer to IPv4-specific structure
+            addr = &(ipv4->sin_addr); // extract 32 bit IPv4 address
+            ipver = "IPv4"; // labelling
         } else { // IPv6
             struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
             addr = &(ipv6->sin6_addr);
@@ -47,6 +54,12 @@ int main(int argc, char *argv[])
         }
 
         inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+        /*
+         * p->ai_family v4/v6
+         * addr points to raw bytes
+         * ipstr is destination buffer
+         * sizeof ipstr safety bound
+         */
         printf("  %s: %s\n", ipver, ipstr);
     }
 
